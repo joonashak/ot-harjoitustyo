b198dc76e738daae8784be97a1400f2e631e3e4c
Joonas Häkkinen <joonas@joonashakkinen.fi>
2019-11-22 23:45:54 +0200

    Refactor main menu to separate class

---
+++ Shamery/src/main/java/fi/basse/shamery/ui/MainMenu.java
@@ -0,0 +1,31 @@
+package fi.basse.shamery.ui;
+
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class MainMenu {
+    Stage primaryStage;
+
+    public MainMenu(Stage primaryStage) {
+        this.primaryStage = primaryStage;    
+    }
+
+    public Scene getScene() {
+        Button newGameButton = new Button("New Game");
+        newGameButton.setOnAction(e -> System.out.println("new game pressed"));
+
+        Button exitButton = new Button("Close");
+        exitButton.setOnAction(e -> {
+            Platform.exit();
+            System.exit(0);
+        });
+
+        VBox vbox = new VBox(10);
+        vbox.getChildren().addAll(newGameButton, exitButton);
+
+        return new Scene(vbox, 300, 200);
+    }
+}
